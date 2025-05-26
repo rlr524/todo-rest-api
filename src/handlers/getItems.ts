92e81f4dfc56333fd91e2bb9d3e584d348c7526e
Rob Ranf <rob@emiyaconsulting.com>
2025-05-26 12:51:21 -0700

    all function config and handler outline

---
+++ src/handlers/getItems.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function handler(event: any) {
+	console.info("Event", event);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: "Get all items" }),
+	};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import express from "express";
+import serverless from "serverless-http";
 import cors from "cors";
 import helmet from "helmet";
 import "dotenv/config";
 import connectDB from "./db.js";
 import ItemService from "./service/itemService.js";
 
-export const handler = () => {
-	const app = express();
-	const port = process.env.PORT;
-	app.use(cors());
-	app.use(helmet());
-	app.use(express.json());
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
-	connectDB().catch((err) => console.error(err));
+connectDB().catch((err) => console.error(err));
 
-	app.get("/", (req, res) => {
-		res.send("Hello, Madison");
-	});
+app.get("/", (req, res) => {
+	res.send("Hello, Madison");
+});
 
-	app.get("/items", ItemService.getItems);
-	app.get("/item/:id", ItemService.getItemById);
-	app.post("/item", ItemService.createItem);
-	app.patch("/item", ItemService.updateItem);
+app.get("/items", ItemService.getItems);
+app.get("/item/:id", ItemService.getItemById);
+app.post("/item", ItemService.createItem);
+app.patch("/item", ItemService.updateItem);
 
-	app.listen(port, () => {
-		console.log(`App listening on port ${port}`);
-	});
-};
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
 
-handler();
+export const handler = serverless(app);
