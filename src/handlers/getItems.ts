987b3ba432919e313ca8be553520a04fb6e469d0
Rob Ranf <rob@emiyaconsulting.com>
2025-05-31 17:45:58 -0700

    completely reworking to use dynamo db and express

--- src/handlers/getItems.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function handler(event: any) {
-	console.info("Event", event);
-
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Get all items" }),
-	};
-}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,15 @@
-import express from "express";
 import serverless from "serverless-http";
-import cors from "cors";
-import helmet from "helmet";
-import "dotenv/config";
-import connectDB from "./db.js";
-import ItemService from "./service/itemService.js";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT;
-app.use(cors());
-app.use(helmet());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-connectDB().catch((err) => console.error(err));
-
-app.get("/", (req, res) => {
-	res.send("Hello, Madison");
-});
-
-app.get("/items", ItemService.getItems);
-app.get("/item/:id", ItemService.getItemById);
-app.post("/item", ItemService.createItem);
-app.patch("/item", ItemService.updateItem);
-app.delete("/item/:id", ItemService.deleteItem);
-
-app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
+app.get("/", function (req: Request, res: Response) {
+	res.send("Welcome to the ToDo API");
 });
 
 export const handler = serverless(app);
