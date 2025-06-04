76017a9f1a64cc57813aeb2351f27ea3dfc47908
Rob Ranf <rob@emiyaconsulting.com>
2025-06-03 17:58:38 -0700

    fixing linter in file validators

--- src/middlewares/validateImage.ts
+++ src/middlewares/validateImage.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { RequestHandler } from "express";
 import MulterRequest from "../types/multerRequest";
 import validateImage from "../utils/validators/image";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const validateImageMiddleware: RequestHandler<Record<string, any>, any, MulterRequest> = (req, res, next) => {
+const validateImageMiddleware: RequestHandler<
+	Record<string, any>,
+	any,
+	MulterRequest
+> = (req, res, next) => {
 	if (req.file) {
 		const { error } = validateImage(req.file);
 		if (error) {
