532c0fa020174c24dfeaeea24076983041870418
Rob Ranf <rob@emiyaconsulting.com>
2025-06-07 16:47:25 -0700

    routes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import router from "./routes/item";
 
 dotenv.config();
 const app = express();
+const version = process.env.API_VERSION
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +14,6 @@ app.get("/", function (req: Request, res: Response) {
 	res.send("Welcome to the ToDo API");
 });
 
-app.use(router);
+app.use(`/api/${version}`, router);
 
 export const handler = serverless(app);
